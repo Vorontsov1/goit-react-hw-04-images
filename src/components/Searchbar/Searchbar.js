7bf79e5a257d6bc8135f3c2f9695703f8f37f00d
Vorontsov1 <evgeniyvorontsov1@gmail.com>
2023-01-03 17:09:07 +0200

    fixed

--- src/components/Searchbar/Searchbar.js
+++ src/components/Searchbar/Searchbar.js
@@ -1,23 +1,45 @@
-import React from 'react';
+import React, { Component } from "react";
 
-const Searchbar = ({onSubmit}) => {
-    return (
-    <header className="Searchbar">
-  <form className='SearchForm' onSubmit={onSubmit}>
-    <button type="submit" className="SearchForm-button">
-      <span className="SearchForm-button-label">Search</span>
-    </button>
 
-    <input
-      className='SearchForm-input'
-      type="text"
-      autoComplete="off"
-      autoFocus
-      placeholder="Шукай більше)"
-      name='searchName'
-    />
-  </form>
-</header>
-    );
-};
-export default Searchbar;
+export default class Searchbar extends Component {
+    state = {
+        imgName: ''
+    }
+
+    handleChangeName = e => {
+        this.setState({ imgName: e.currentTarget.value.toLowerCase() })
+    };
+    handleSubmit = e => {
+        e.preventDefault();
+        this.props.onSubmit(this.state.imgName);
+        this.setState({ imgName: '' })
+    
+    };
+
+
+    render() { 
+        return (
+            <header className="Searchbar">
+                <form
+                    className='SearchForm'
+                    onSubmit={this.handleSubmit}>
+                  <button type="submit" className="SearchForm-button">
+                    <span className="SearchForm-button-label">Search</span>
+                  </button>
+
+
+                    <input
+                        className='SearchForm-input'
+                        type="text"
+                        autoComplete="off"
+                        autoFocus
+                        placeholder="Search images and photos"
+                        onChange={this.handleChangeName}
+                        value={this.state.imgName}
+                    />
+                </form>
+            </header>
+        );
+    }
+        
+}
