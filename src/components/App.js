643a3c93130ee070707b56e3227613975e13d745
Vorontsov1 <evgeniyvorontsov1@gmail.com>
2023-01-08 18:41:46 +0200

    done

--- src/components/App.js
+++ src/components/App.js
@@ -1,78 +1,4 @@
-// import React, { useState, useEffect } from 'react';
-// import Button from './Button/Button';
-// import ImageGallery from './ImageGallery/ImageGallery';
-// import Loader from './Loader/Loader';
-// import {Searchbar} from './Searchbar/Searchbar';
-// import {Modal} from './Modal/Modal';
-// import  * as API from '../services/api';
-// import './App.css';
-
-
-
-// export const App = () => {
-//  const [page, setPage] = useState(1);
-//  const [searchName, setSearchName] = useState('');
-//  const [largeImage, setLargeImage] = useState('');
-//  const [items, setItems] = useState([]);
-//  const [isLoading, setIsLoading] = useState(false);
-//  const [error, setError] = useState(null);
-
-
-
-
-//   useEffect(() => {
-//    if(searchName !== '') {
-//     getImages(searchName, page);
-//    }
-//   }, [searchName, page])
-
-
-//   const handleFormSubmit = (newSearchName) => {
-//     if(newSearchName.trim().length === 0) {
-//       alert('Please, enter request');
-//       return
-//     }
-//     setSearchName(newSearchName);
-//     setPage(1);
-//     setItems([]);
-//     }
-
-
-//   const getImages = async (searchName, page) => {
-//     try {
-//       setIsLoading(true);
-//       const images = await API.loadImage(searchName, page);
-//       setItems(prevState => [...prevState, ...images]);
-//       setIsLoading(false);
-      
-//       if (images.length === 0) {
-//         alert("Sorry, we can't find anyting for your request. Please, enter another request");
-//         }
-//       } catch (error) {
-//         setError(error.message);
-//         } finally {
-//         setIsLoading(false) 
-//       }
-//     };
-    
-  
- 
-
-//     return (
-//       <div className="App">
-//       <Searchbar onSubmit={handleFormSubmit} isLoading={isLoading}/>
-//       {error && <p>{error}</p>}
-//        {items.length > 0 &&  <ImageGallery items={items}  onClick={setLargeImage} />}
-//        {isLoading && <Loader />}
-//        {items.length >= && <Button onLoadMore={() => setPage(prev => prev + 1)} isLoading={isLoading}/>}
-//        {largeImage && (<Modal onClose={() => setLargeImage('')} url={largeImage} />)}
-//      </div>
-//     );
-//   };
-
-
-
-//   import React, { Component } from 'react';
+// import React, { Component } from 'react';
 // import Button from './Button/Button';
 // import ImageGallery from './ImageGallery/ImageGallery';
 // import Loader from './Loader/Loader';
@@ -199,7 +125,8 @@ import './App.css';
       return
     }
     getImages(searchName, page);
-       // eslint-disable-next-line react-hooks/exhaustive-deps
+       
+  // eslint-disable-next-line react-hooks/exhaustive-deps
   }, [page, searchName]);
 
   const getImages = async (searchName, page) => {
